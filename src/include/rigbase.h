bf068ba8682aac09fcd6f8ac143e4858ea4d07f9
Bruce McLean <mcleanb@photo-nomad.net>
2026-04-20 07:30:55 -0400

    add clarifier operations to FTX1

--- src/include/rigbase.h
+++ src/include/rigbase.h
@@ -795,6 +795,10 @@ double vfo_;
 	virtual void change_channel(bool channel_up) {}
 	virtual void scan_operation(bool start) {}
 	virtual bool is_in_memory_mode() { return false; }
+	virtual void set_rx_clarifier_state(bool start) {}
+	virtual bool get_rx_clarifier_state() { return false; }
+	virtual void set_rx_clarifier_value(int level) {}
+	virtual int get_rx_clarifier_value() { return 0; }
 
 	virtual bool read_rx_dual() { return false; }
 	virtual void set_rx_dual(bool dual) {}
--- src/include/yaesu/FTX1.h
+++ src/include/yaesu/FTX1.h
@@ -185,6 +185,10 @@ public:
 	virtual void scan_operation(bool start);
 	virtual bool is_in_memory_mode();
 	virtual void power(bool on);
+    virtual void set_rx_clarifier_state(bool start);
+    virtual bool get_rx_clarifier_state();
+    virtual void set_rx_clarifier_value(int level);
+    virtual int get_rx_clarifier_value();
 
 	virtual bool read_rx_dual();
 	virtual void set_rx_dual(bool dual);
